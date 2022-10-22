438c243fbab0fac5ccce9e1b2e8c3f3ed1b25414
Kate <ekatromanova90@mail.ru>
2022-10-22 04:12:40 +0300

    z3

---
+++ Z3/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+void InputMatrix(int[,] matrix)
+{
+    
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        if(i%2==0)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                matrix[i, j] = j+i*matrix.GetLength(1);
+                Console.Write($"{matrix[i, j]} \t");
+            }
+        }
+        if(i%2==1)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                matrix[i, j] =(i+1)*matrix.GetLength(1)-(j+1);
+                Console.Write($"{matrix[i, j]} \t");
+            }
+        }
+        
+        Console.WriteLine();
+        
+    }
+}
+Console.Clear();
+Console.Write("Введите кол-во строк: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[n, m];
+InputMatrix(matrix);
